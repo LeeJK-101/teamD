697aa81ea4b9429ecae7a8e2920d698ad8938274
bug0630 <bugbdh@gmail.com>
2024-03-25 22:57:40 +0900

    header debug

--- js/header.js
+++ js/header.js
@@ -1,23 +1,26 @@
 const searchIcon = document.querySelector('#searchIcon'),
-  navOpenBtn = document.querySelector('.navOpenBtn'),
-  nav = document.querySelector('.nav'),
+  burger = document.querySelector('#burger'),
+  hiddenBtn = document.querySelector('.hidden'),
+  nav = document.querySelector('.nomalNav'),
+  modal = document.querySelector('.modal'),
+  modalBack = document.querySelector('.modalBack'),
+  searchBox = document.querySelector('.search-box'),
   navCloseBtn = document.querySelector('.navCloseBtn');
 
 searchIcon.addEventListener('click', () => {
-  nav.classList.toggle('openSearch');
-  nav.classList.remove('openNav');
-  if (nav.classList.contains('openSearch')) {
-    return searchIcon.classList.replace('search', 'uil-times');
-  }
-  searchIcon.classList.replace('uil-times', 'search');
+  nav.classList.toggle('hidden');
+  searchBox.classList.toggle('hidden');
 });
-// --------------------------------------------------
+burger.addEventListener('click', () => {
+  modal.classList.toggle('hidden');
+});
+modalBack.addEventListener('click', () => {
+  modal.classList.toggle('hidden');
+});
+const searchBtn = document.querySelector('#searchBtn');
 
-navOpenBtn.addEventListener('click', () => {
-  nav.classList.add('openNav');
-  nav.classList.remove('openSearch');
-  searchIcon.classList.replace('uil-times', 'search');
-});
-navCloseBtn.addEventListener('click', () => {
-  nav.classList.remove('openNav');
+searchBtn.addEventListener('click', () => {
+  const keyword = document.querySelector('input').value;
+  // -------나중에 search입력값을 쿼리스트링으로 보내 이동 @@지우지 마세요@@
+  // window.location.href = `search.html?class=${keyword}`;
 });
